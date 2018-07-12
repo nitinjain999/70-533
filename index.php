dcafeb06e8188aa4f7bae95784bd04103b725215
Nitin Jain <nitin_jain999@yahoo.co.in>
2018-07-12 20:43:50 +0200

    Update index.php
    Update index.php in Dev Branch

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello Saurabh & Deepu its my first AZ Webapp</p>'; ?> 
+ <?php echo '<p>Hello Saurabh & Deepu its my first AZ Webapp from Dev Slot</p>'; ?> 
  </body>
 </html>
