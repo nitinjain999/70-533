a062d862efb9855c71fe0884c5528429e09dd8d0
Nitin Jain <nitin_jain999@yahoo.co.in>
2018-07-12 20:51:28 +0200

    Index.php Staging Slot

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello Saurabh & Deepu its my first AZ Webapp from Dev Slot</p>'; ?> 
+ <?php echo '<p>Hello Saurabh & Deepu its my first AZ Webapp from Staging Slot</p>'; ?> 
  </body>
 </html>
