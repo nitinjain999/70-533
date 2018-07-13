736bb472ab04ed28ea303b848ec8d4d0a11121d6
Deepu Venugopal <deepu.venugopal@ericsson.com>
2018-07-13 10:05:53 +0200

    testing the edevenu sub dir

--- edevenu/index.php
+++ edevenu/index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello First commit edevenu</p>'; ?> 
+ <?php echo '<p>Hello.. I modified for these changes to test webapp with git sub dir</p>'; ?> 
  </body>
 </html>
